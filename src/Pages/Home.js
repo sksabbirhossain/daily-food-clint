a2f1df43514519e62ef6e40db1eb21dcdd3dd317
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-11-07 23:41:49 +0600

    created a top menu successfully

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            <h3 className='text-center'>home page</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="text-center">home page</h3>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../images/logo.png";
+import styles from "./Header.module.css";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(true);
+
   return (
-    <div>
-      <h3>header</h3>
-    </div>
+    <nav className={`navbar navbar-expand-lg shadow-sm ${styles.mainNav}`}>
+      <div className="container">
+        <Link className="navbar-brand p-0" to="/">
+          <div className="d-none d-sm-flex">
+            <img
+              src={logo}
+              className={`${styles.logo}`}
+              alt="daily food logo"
+            />
+          </div>
+          <div className="d-flex d-sm-none">
+            <img
+              src={logo}
+              className={`${styles.mobileLogo}`}
+              alt="daily food logo"
+            />
+          </div>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${
+            mobileMenu ? "" : styles.mobileMenu
+          }`}
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className={`${styles.navLink}`} to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={`${styles.navLink}`} to="/blog">
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={`${styles.navLink}`} to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={`${styles.navLink}`} to="/my-reviews">
+                My reviews
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={`${styles.navLink}`} to="/add-service">
+                Add service
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={`${styles.navLink}`} to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <span className={`${styles.navLink}`}>LogOut</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
