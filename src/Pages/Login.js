a8541ec5061ae2ed923d78d5adac0902b4c62983
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-11-08 20:43:35 +0600

    created a function for user login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { FaArrowRight } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Button from '../components/Button/Button';
 import Form from '../components/Form/Form';
 import FormInput from '../components/FormInput/FormInput';
+import { useAuth } from '../contexts/AuthContext';
 import styles from "../styles/Login.module.css"
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("")
+  const { userLogin, googleSignin } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+  // login user
+  const handleUserLogin = (e) => {
+    e.preventDefault();
+    userLogin(email, password)
+      .then((userInfo) => {
+        const user = userInfo.user;
+        console.log(user)
+        navigate(from, { replace: true });
+        toast.success("Login successful");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
+  };
+
+
+      // google signup function
+      const handleGoogleSignIn = () => {
+        googleSignin()
+          .then((result) => {
+            const user = result.user;
+            console.log(user);
+            toast.success("Google SignUp successful");
+            navigate("/");
+          })
+          .catch((error) => {
+            const errorMessage = error.message;
+            toast.error(errorMessage);
+          });
+      };
+  
     return (
         <section className="mt-4 mt-md-5 ">
       <div className="container">
@@ -17,14 +59,20 @@ const Login = () => {
           <div className="row">
             <div className="col-md-4 offset-md-4">
               <div className="card p-3 mb-4 shadow">
-                <Form >
-                  <FormInput label="User Email" type="email" name="email" placeholder="user email address" required/>
-                  <FormInput label="Password" type="password" name="password" placeholder="password" required/>
+                <Form onSubmit={handleUserLogin}>
+                  <FormInput label="User Email" type="email" name="email" placeholder="user email address" value={email}
+                  onChange={(e) => setEmail(e.target.value)} required/>
+                  
+                  <FormInput label="Password" type="password" name="password" placeholder="password" value={password}
+                  onChange={(e) => setPassword(e.target.value)} required/>
+                  
+                  <p className='text-danger fw-bold'>{ error}</p>
+
                   <Button className="mt-2">Login <FaArrowRight/> </Button>
                 </Form>
                 <span className='py-2 text-center'>OR</span>
                 <div className='shadow py-3 text-center rounded'>
-                <span className={styles.loginIcons}>Login With Google</span>
+                <span onClick={handleGoogleSignIn} className={styles.loginIcons}>Login With Google</span>
                 </div>
                 <p className="mt-4">
                     <small>
